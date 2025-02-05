a5c77c0153c18564b56d81424df23f8576b37553
Gourav-patil-git <gouravpatil654@gmail.com>
2025-02-05 14:19:18 +0530

    Database connection done

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+const db =new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    password:"root",
+    database:"world",
+    port:5432,
+});
 
+const app= express();
+const port=3000;
+
+db.connect();
+let quiz=[];
+
+db.query("SELECT * FROM CAPITALS" ,(err ,res)=>{
+    if (err) app.send("Internal server error");
+    else quiz = res.rows;
+
+db.end();
+});
 
 app.listen(port ,()=>{
     console.log(`server is running at http://localhost:${port} `);
