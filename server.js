a2f2b459b0f63188844b39da323488da41ec28ed
Gourav-patil-git <gouravpatil654@gmail.com>
2025-02-05 14:07:34 +0530

    World Capital Quiz Frontend done Backend Start

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+
+
+app.listen(port ,()=>{
+    console.log(`server is running at http://localhost:${port} `);
+})
